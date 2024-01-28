4315adf20a8fddeeac172ba9b10d85c27a2bc250
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-01-28 17:21:25 +0800

    chore: game scense

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,22 @@
 use bevy::prelude::*;
 
-pub struct GamePlugin;
+use crate::GameState;
 
 #[allow(unused)]
+enum Color {
+    Red,
+    Blue,
+}
+
+pub struct GamePlugin;
+
 impl Plugin for GamePlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Game), set_up_game_data);
+    }
+}
+
+#[allow(unused)]
+fn set_up_game_data(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font: Handle<Font> = asset_server.load("fonts/FiraSans-Bold.ttf");
 }
